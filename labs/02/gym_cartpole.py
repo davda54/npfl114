7efd97fcbf1b7f6436a5a8d315e7bdf437f5bf1a
David Samuel <david.samuel@seznam.cz>
2019-03-16 12:09:42 +0100

    Gym cartpole homework

--- labs/02/gym_cartpole.py
+++ labs/02/gym_cartpole.py
@@ -10,10 +10,42 @@ import tensorflow as tf
 # Parse arguments
 # TODO: Set reasonable defaults and possibly add more arguments.
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--activation", default="tanh", type=str, help="Activation function.")
+parser.add_argument("--batch_size", default=1, type=int, help="Batch size.")
+parser.add_argument("--decay", default='exponential', type=str, help="Learning decay rate type")
+parser.add_argument("--epochs", default=100, type=int, help="Number of epochs.")
+parser.add_argument("--hidden_layer", default=4, type=int, help="Size of the hidden layer.")
+parser.add_argument("--learning_rate", default=0.01, type=float, help="Initial learning rate.")
+parser.add_argument("--learning_rate_final", default=0.001, type=float, help="Final learning rate.")
+parser.add_argument("--momentum", default=None, type=float, help="Momentum.")
+parser.add_argument("--optimizer", default="Adam", type=str, help="Optimizer to use.")
+parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
+parser.add_argument("--threads", default=4, type=int, help="Maximum number of threads to use.")
 args = parser.parse_args()
+
+activation_dict = {"none": None, "relu": tf.nn.relu, "tanh": tf.nn.tanh, "sigmoid": tf.nn.sigmoid}
+
+
+class LearningRateTensorBoard(tf.keras.callbacks.TensorBoard):
+    def __init__(self, log_dir='./logs', histogram_freq=0, write_graph=True, write_images=False, update_freq='epoch', profile_batch=2):
+        super().__init__(log_dir, histogram_freq, write_graph, write_images, update_freq, profile_batch)
+
+    def on_batch_end(self, batch, logs=None):
+        logs.update({'learning rate': args.learning_rate if args.decay is None else float(model.optimizer.learning_rate(model.optimizer.iterations))})
+        super().on_batch_end(batch, logs)
+
+
+def learning_rate(decay):
+    decay_steps = args.epochs * 100 // args.batch_size
+    if decay is None:            return args.learning_rate
+    elif decay == 'polynomial':  return tf.optimizers.schedules.PolynomialDecay(args.learning_rate, decay_steps, args.learning_rate_final)
+    elif decay == 'exponential': return tf.optimizers.schedules.ExponentialDecay(args.learning_rate, decay_steps, args.learning_rate_final / args.learning_rate)
+
+
+def optimizer(type):
+    if type == 'SGD':    return tf.optimizers.SGD(learning_rate=learning_rate(args.decay), momentum=0.0 if args.momentum is None else args.momentum)
+    elif type == 'Adam': return tf.optimizers.Adam(learning_rate=learning_rate(args.decay))
+
 
 # Fix random seeds
 np.random.seed(42)
@@ -22,7 +54,7 @@ tf.config.threading.set_inter_op_parallelism_threads(args.threads)
 tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
 # Create logdir name
-args.logdir = "logs/{}-{}-{}".format(
+args.logdir = "cartpole_logs\\{}-{}-{}".format(
     os.path.basename(__file__),
     datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
     ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
@@ -41,15 +73,19 @@ observations, labels = np.array(observations), np.array(labels)
 # However, beware that there is currently a bug in Keras which does
 # not correctly serialize InputLayer. Instead of using an InputLayer,
 # pass explicitly `input_shape` to the first real model layer.
-model = None
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(args.hidden_layer, activation=activation_dict[args.activation], input_shape=(4,)),
+    #tf.keras.layers.Dense(args.hidden_layer, activation=activation_dict[args.activation]),
+    tf.keras.layers.Dense(2, activation=tf.nn.softmax)])
 
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(),
+    optimizer=optimizer(args.optimizer),
     loss=tf.keras.losses.SparseCategoricalCrossentropy(),
     metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
 )
 
-tb_callback=tf.keras.callbacks.TensorBoard(args.logdir)
+tb_callback = LearningRateTensorBoard(args.logdir, histogram_freq=1, update_freq=100, profile_batch=1)
+tb_callback.on_train_end = lambda *_: None
 model.fit(observations, labels, batch_size=args.batch_size, epochs=args.epochs, callbacks=[tb_callback])
 
 model.save("gym_cartpole_model.h5", include_optimizer=False)
