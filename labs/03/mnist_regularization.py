d36c0cbcf53bd3dea1a866f71ebb1cfa10bb789d
David Samuel <david.samuel@seznam.cz>
2019-03-18 17:00:30 +0100

    MNIST regularization homework

--- labs/03/mnist_regularization.py
+++ labs/03/mnist_regularization.py
@@ -22,6 +22,9 @@ parser.add_argument("--threads", default=1, type=int, help="Maximum number of th
 args = parser.parse_args()
 args.hidden_layers = [int(hidden_layer) for hidden_layer in args.hidden_layers.split(",") if hidden_layer]
 
+def sparse_to_distribution(labels):
+    return np.eye(mnist.LABELS)[labels]
+
 # Fix random seeds
 np.random.seed(42)
 tf.random.set_seed(42)
@@ -46,6 +49,11 @@ mnist = MNIST()
 # because of a bug if `args.l2` is zero, use `None` instead of `L1L2` regularizer
 # with zero l2.
 
+if args.l2 is not None and args.l2 != 0:
+    regularizer = tf.keras.regularizers.L1L2(l2=args.l2)
+else:
+    regularizer = None
+
 # TODO: Implement dropout.
 # Add a `tf.keras.layers.Dropout` with `args.dropout` rate after the Flatten
 # layer and after each Dense hidden layer (but not after the output Dense layer).
@@ -53,14 +61,16 @@ mnist = MNIST()
 # Create the model
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=[MNIST.H, MNIST.W, MNIST.C]))
+model.add(tf.keras.layers.Dropout(args.dropout))
 for hidden_layer in args.hidden_layers:
-    model.add(tf.keras.layers.Dense(hidden_layer, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(MNIST.LABELS))
+    model.add(tf.keras.layers.Dense(hidden_layer, activation=tf.nn.relu, kernel_regularizer=regularizer, bias_regularizer=regularizer))
+    model.add(tf.keras.layers.Dropout(args.dropout))
+model.add(tf.keras.layers.Dense(MNIST.LABELS, kernel_regularizer=regularizer, bias_regularizer=regularizer))
 
 # TODO: Implement label smoothing.
 # Apply the given smoothing. You will need to change the
 # SparseCategorical{Crossentropy,Accuracy} to Categorical{Crossentropy,Accuracy}
-# because label_smooting is supported only by CategoricalCrossentropy.
+# because label_smoothing is supported only by CategoricalCrossentropy.
 # That means you also need to modify the labels of all three datasets
 # (i.e., mnist.{train,dev,test}.data["labels"]) from indices of the gold class
 # to a full categorical distribution (you can use either NumPy or there is
@@ -68,20 +78,20 @@ model.add(tf.keras.layers.Dense(MNIST.LABELS))
 
 model.compile(
     optimizer=tf.keras.optimizers.Adam(),
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
+    loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True, label_smoothing=args.label_smoothing),
+    metrics=[tf.keras.metrics.CategoricalAccuracy(name="accuracy")],
 )
 
 tb_callback=tf.keras.callbacks.TensorBoard(args.logdir, update_freq=1000, profile_batch=1)
 tb_callback.on_train_end = lambda *_: None
 model.fit(
-    mnist.train.data["images"][:5000], mnist.train.data["labels"][:5000],
+    mnist.train.data["images"][:5000], sparse_to_distribution(mnist.train.data["labels"][:5000]),
     batch_size=args.batch_size, epochs=args.epochs,
-    validation_data=(mnist.dev.data["images"], mnist.dev.data["labels"]),
+    validation_data=(mnist.dev.data["images"], sparse_to_distribution(mnist.dev.data["labels"])),
     callbacks=[tb_callback],
 )
 
-test_logs = model.evaluate(mnist.test.data["images"], mnist.test.data["labels"], batch_size=args.batch_size)
+test_logs = model.evaluate(mnist.test.data["images"], sparse_to_distribution(mnist.test.data["labels"]), batch_size=args.batch_size)
 tb_callback.on_epoch_end(1, dict(("val_test_" + metric, value) for metric, value in zip(model.metrics_names, test_logs)))
 
 accuracy = test_logs[model.metrics_names.index("accuracy")]
