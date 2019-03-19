a4b0ad32893e499a770224b311e76cc0a1fd43c7
David Samuel <david.samuel@seznam.cz>
2019-03-19 15:59:49 +0100

    Uppercase baseline 97.67 %

--- labs/03/uppercase.py
+++ labs/03/uppercase.py
@@ -11,12 +11,23 @@ from uppercase_data import UppercaseData
 
 # Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--alphabet_size", default=None, type=int, help="If nonzero, limit alphabet to this many most frequent chars.")
-parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=10, type=int, help="Number of epochs.")
-parser.add_argument("--hidden_layers", default="500", type=str, help="Hidden layer configuration.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-parser.add_argument("--window", default=None, type=int, help="Window size to use.")
+parser.add_argument("--activation", default="relu", type=str, help="Activation function.")
+parser.add_argument("--alphabet_size", default=80, type=int, help="If nonzero, limit alphabet to this many most frequent chars.")
+parser.add_argument("--batch_size", default=1024, type=int, help="Batch size.")
+parser.add_argument("--decay", default='exponential', type=str, help="Learning decay rate type")
+parser.add_argument("--dropout", default=0.5, type=float, help="Dropout regularization.")
+parser.add_argument("--embedding_size", default=64, type=float, help="Dimension of the embedding layer.")
+parser.add_argument("--embedding_dropout", default=0.0, type=float, help="Embedding dropout regularization.")
+parser.add_argument("--epochs", default=20, type=int, help="Number of epochs.")
+parser.add_argument("--hidden_layers", default="512", type=str, help="Hidden layer configuration.")
+parser.add_argument("--label_smoothing", default=0, type=float, help="Label smoothing.")
+parser.add_argument("--learning_rate", default=0.01, type=float, help="Initial learning rate.")
+parser.add_argument("--learning_rate_final", default=0.001, type=float, help="Final learning rate.")
+parser.add_argument("--momentum", default=None, type=float, help="Momentum.")
+parser.add_argument("--optimizer", default="Adam", type=str, help="Optimizer to use.")
+parser.add_argument("--threads", default=8, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--window", default=6, type=int, help="Window size to use.")
+
 args = parser.parse_args()
 args.hidden_layers = [int(hidden_layer) for hidden_layer in args.hidden_layers.split(",") if hidden_layer]
 
@@ -35,6 +46,19 @@ args.logdir = os.path.join("logs", "{}-{}-{}".format(
 
 # Load data
 uppercase_data = UppercaseData(args.window, args.alphabet_size)
+
+def learning_rate(decay):
+    decay_steps = args.epochs * uppercase_data.train.size // args.batch_size
+    if decay is None: return args.learning_rate
+    elif decay == 'polynomial': return tf.optimizers.schedules.PolynomialDecay(args.learning_rate, decay_steps, args.learning_rate_final)
+    elif decay == 'exponential': return tf.optimizers.schedules.ExponentialDecay(args.learning_rate, decay_steps, args.learning_rate_final / args.learning_rate)
+
+def optimizer(type):
+    if type == 'SGD':return tf.optimizers.SGD(learning_rate=learning_rate(args.decay), momentum=0.0 if args.momentum is None else args.momentum)
+    elif type == 'Adam': return tf.optimizers.Adam(learning_rate=learning_rate(args.decay))
+
+def sparse_to_distribution(labels):
+    return np.eye(uppercase_data.LABELS)[labels]
 
 # TODO: Implement a suitable model, optionally including regularization, select
 # good hyperparameters and train the model.
@@ -55,6 +79,32 @@ uppercase_data = UppercaseData(args.window, args.alphabet_size)
 #   You can then flatten the one-hot encoded windows and follow with a dense layer.
 # - Alternatively, you can use `tf.keras.layers.Embedding`, which is an efficient
 #   implementation of one-hot encoding followed by a Dense layer, and flatten afterwards.
+
+# Create the model
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Embedding(args.alphabet_size, args.embedding_size, input_length=2*args.window + 1))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dropout(args.embedding_dropout))
+for hidden_layer in args.hidden_layers:
+    model.add(tf.keras.layers.Dense(hidden_layer, activation=args.activation))
+    model.add(tf.keras.layers.Dropout(args.dropout))
+model.add(tf.keras.layers.Dense(uppercase_data.LABELS))
+
+model.compile(
+    optimizer=optimizer(args.optimizer),
+    loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True, label_smoothing=args.label_smoothing) if args.label_smoothing > 0 else tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[tf.keras.metrics.CategoricalAccuracy(name="accuracy") if args.label_smoothing > 0 else tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
+)
+
+tb_callback=tf.keras.callbacks.TensorBoard(args.logdir, update_freq=1000, profile_batch=1)
+tb_callback.on_train_end = lambda *_: None
+model.fit(
+    uppercase_data.train.data["windows"], sparse_to_distribution(uppercase_data.train.data["labels"]) if args.label_smoothing > 0 else uppercase_data.train.data["labels"],
+    batch_size=args.batch_size, epochs=args.epochs,
+    validation_data=(uppercase_data.dev.data["windows"], sparse_to_distribution(uppercase_data.dev.data["labels"]) if args.label_smoothing > 0 else uppercase_data.dev.data["labels"]),
+    callbacks=[tb_callback],
+)
+
 
 with open("uppercase_test.txt", "w") as out_file:
     # TODO: Generate correctly capitalized test set.
