555f0c644e3024c9fe73786e84615996bd431803
David Samuel <david.samuel@seznam.cz>
2019-04-25 19:42:34 +0200

    speech recognition homework

--- labs/07/speech_recognition.py
+++ labs/07/speech_recognition.py
@@ -3,12 +3,17 @@ import contextlib
 
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.layers import LSTM, GRU, Bidirectional, Embedding, Dense, Lambda, concatenate
 
 from timit_mfcc import TimitMFCC
+
+tf.config.gpu.set_per_process_memory_growth(True)
+
 
 class Network:
     def __init__(self, args):
         self._beam_width = args.ctc_beam
+        self.best_distance = 1.0
 
         # TODO: Define a suitable model, given already masked `mfcc` with shape
         # `[None, TimitMFCC.MFCC_DIM]`. The last layer should be a Dense layer
@@ -16,10 +21,20 @@ class Network:
         # where the `+ 1` is for the CTC blank symbol.
         #
         # Store the results in `self.model`.
+        hidden = input = tf.keras.Input(shape=[None, TimitMFCC.MFCC_DIM], dtype=tf.float32)
+
+        for _ in range(args.rnn_layers):
+            if args.rnn_cell == 'LSTM':
+                hidden = Bidirectional(LSTM(units=args.rnn_dim, return_sequences=True))(hidden)
+            elif args.rnn_cell == 'GRU':
+                hidden = Bidirectional(GRU(units=args.rnn_dim, return_sequences=True))(hidden)
+
+        predictions = Dense(units=len(TimitMFCC.LETTERS) + 1, activation=None)(hidden)
+
+        self.model = tf.keras.Model(inputs=input, outputs=predictions)
 
         # The following are just defaults, do not hesitate to modify them.
         self._optimizer = tf.optimizers.Adam()
-        self._loss = tf.losses.SparseCategoricalCrossentropy()
         self._metrics = {"loss": tf.metrics.Mean(), "edit_distance": tf.metrics.Mean()}
         self._writer = tf.summary.create_file_writer(args.logdir, flush_millis=10 * 1000)
 
@@ -63,25 +78,53 @@ class Network:
                                   tf.TensorSpec(shape=[None], dtype=tf.int32),
                                   tf.TensorSpec(shape=[None, None], dtype=tf.int32)])
     def train_batch(self, mfcc, mfcc_lens, targets):
-        # TODO: Implement batch training.
-        pass
+        sparse_targets = self._to_sparse(tf.cast(targets, dtype=tf.int32))
 
-    def train_epoch(self, dataset, args):
-        # TODO: Store suitable metrics in TensorBoard
-        for batch in dataset.batches(args.batch_size):
+        with tf.GradientTape() as tape:
+            logits = self._compute_logits(mfcc, mfcc_lens, training=True)
+            loss = self._ctc_loss(logits, mfcc_lens, sparse_targets)
+
+        gradients = tape.gradient(loss, self.model.variables)
+
+        if args.clip_gradient is not None:
+            gradients, _ = tf.clip_by_global_norm(gradients, args.clip_gradient)
+        self._optimizer.apply_gradients(zip(gradients, self.model.variables))
+
+        self._edit_distance(self._ctc_predict(logits, mfcc_lens), sparse_targets)
+
+    def train_epoch(self, epoch, dataset, args):
+        for _, metric in self._metrics.items():
+            metric.reset_states()
+
+        batches_num = dataset.size / args.batch_size
+
+        for i, batch in enumerate(dataset.batches(args.batch_size)):
             self.train_batch(batch["mfcc"], batch["mfcc_len"], batch["letters"])
+
+            print(f'\repoch: {epoch:3d} | train loss: {self._metrics["loss"].result():3.2f} | train edit dist: {self._metrics["edit_distance"].result():.4f} | {int(i / batches_num * 100)} %', end='', flush=True)
+        print(f'\repoch: {epoch:3d} | train loss: {self._metrics["loss"].result():3.2f} | train edit dist: {self._metrics["edit_distance"].result():.4f} | ', end='', flush=True)
 
     @tf.function(input_signature=[tf.TensorSpec(shape=[None, None, TimitMFCC.MFCC_DIM], dtype=tf.float32),
                                   tf.TensorSpec(shape=[None], dtype=tf.int32),
                                   tf.TensorSpec(shape=[None, None], dtype=tf.int32)])
     def evaluate_batch(self, mfcc, mfcc_lens, targets):
-        # TODO: Implement batch evaluation.
-        pass
+        sparse_targets = self._to_sparse(targets)
+
+        logits = self._compute_logits(mfcc, mfcc_lens, training=True)
+        self._edit_distance(self._ctc_predict(logits, mfcc_lens), sparse_targets)
 
     def evaluate(self, dataset, dataset_name, args):
-        # TODO: Store suitable metrics in TensorBoard
+        for _, metric in self._metrics.items():
+            metric.reset_states()
+
         for batch in dataset.batches(args.batch_size):
             self.evaluate_batch(batch["mfcc"], batch["mfcc_len"], batch["letters"])
+
+        print(f'eval edit dist: {self._metrics["edit_distance"].result():.4f}', flush=True)
+
+        if self._metrics["edit_distance"].result() < self.best_distance:
+            self.best_distance = self._metrics["edit_distance"].result()
+            self.model.save_weights(f"models/{args.rnn_cell}-{args.rnn_layers}-{args.rnn_dim}_acc-{self.best_distance:.4f}")
 
     @tf.function(input_signature=[tf.TensorSpec(shape=[None, None, TimitMFCC.MFCC_DIM], dtype=tf.float32),
                                   tf.TensorSpec(shape=[None], dtype=tf.int32)])
@@ -106,10 +149,14 @@ if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
+    parser.add_argument("--batch_size", default=8, type=int, help="Batch size.")
     parser.add_argument("--ctc_beam", default=16, type=int, help="CTC beam.")
-    parser.add_argument("--epochs", default=10, type=int, help="Number of epochs.")
-    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+    parser.add_argument("--epochs", default=100, type=int, help="Number of epochs.")
+    parser.add_argument("--rnn_dim", default=128, type=int, help="Size of LSTM layer.")
+    parser.add_argument("--rnn_cell", default="GRU", type=str, help="RNN cell type.")
+    parser.add_argument("--rnn_layers", default=3, type=int, help="number of RNN layers.")
+    parser.add_argument("--threads", default=4, type=int, help="Maximum number of threads to use.")
+    parser.add_argument("--clip_gradient", default='0.1', type=float)
     args = parser.parse_args()
 
     # Fix random seeds and number of threads
@@ -130,8 +177,9 @@ if __name__ == "__main__":
 
     # Create the network and train
     network = Network(args)
+    # network.model.load_weights("model/GRU-3-128_acc-0.5991")
     for epoch in range(args.epochs):
-        network.train_epoch(timit.train, args)
+        network.train_epoch(epoch, timit.train, args)
         network.evaluate(timit.dev, "dev", args)
 
     # Generate test set annotations, but to allow parallel execution, create it
